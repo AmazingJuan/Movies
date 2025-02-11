8facc9d5ef7f191f5b872a0986e34722376deece
Juan Avendaño <jpavendanb@eafit.edu.co>
2025-02-11 00:11:59 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html', {'name': 'Juan Pablo de Jesus Avendaño Bustamante'} )
+    return render(request, 'home.html', {'name': 'Juan Pablo de Jesús Avendaño Bustamante'} )
 
 def about(request):
     return HttpResponse("<h1> Welcome to About Page</h1>")
